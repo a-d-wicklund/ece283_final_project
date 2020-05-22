d45f56542d41cdb2fee424084be3f130fe349135
adw <alecwick@yahoo.com>
2020-05-22 01:13:13 -0700

    apply changes to test_midi

--- test_midi.py
+++ test_midi.py
@@ -4,19 +4,11 @@ from mido import MidiFile, MidiTrack, Message
 import midi
 
 
-samples = midi.midi_to_samples('piano_concerto.mid')
+samples = midi.midi_to_samples('C:\\Users\\alecw\\Documents\\UCSB\\2019-2020\\ECE 283\\130000_Pop_Rock_Classical_Videogame_EDM_MIDI_Archive[6_19_15]\\0\\009count.mid')
 
-samples1 = np.array(samples[0])
-print(samples1.shape)
 
-for i, inst in enumerate(samples):
-    for j, measure in enumerate(inst):
-        if np.all(measure == 0):
-            samples[i] = np.delete(samples[i], j, axis=0)
-print('after reduction samples shape is {}'.format(samples.shape))
-
-for i in range(3*3):
+for i, k in enumerate(range(3*3)):
     plt.subplot(3,3,i+1)
-    plt.imshow(samples1[i])
-    plt.title("measure {}".format(i+1))
+    plt.imshow(samples[0][k])
+    plt.title("measure {}".format(k+1))
 plt.show()
